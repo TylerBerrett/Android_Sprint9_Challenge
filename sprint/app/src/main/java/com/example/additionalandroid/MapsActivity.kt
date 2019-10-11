cd1f01e8dcacf79bc60e9389d037240e1ac9d9d9
Tyler Berrett <btylerberrett@gmail.com>
2019-10-11 09:46:41 -0600

    add menu add request location add menu buttons add pin and center location add functionality to menu buttons

--- sprint/app/src/main/java/com/example/additionalandroid/MapsActivity.kt
+++ sprint/app/src/main/java/com/example/additionalandroid/MapsActivity.kt
@@ -1,18 +1,30 @@
 package com.example.additionalandroid
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import java.util.jar.Manifest
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+
+    private lateinit var centerLatLong: LatLng
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +33,17 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        val permission = android.Manifest.permission.ACCESS_FINE_LOCATION
+        if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(permission), 12)
+        } else{
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        }
     }
+
+
 
     /**
      * Manipulates the map once available.
@@ -35,9 +57,31 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
+        fusedLocationClient.lastLocation.addOnSuccessListener {
+            val currentLocation = LatLng(it.latitude, it.longitude)
+            centerLatLong = currentLocation
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation))
+        }
+
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.action_bar, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.drop_pin -> {
+                mMap.addMarker(MarkerOptions().position(mMap.cameraPosition.target).title("My Marker"))
+            }
+            R.id.center_location -> mMap.moveCamera(CameraUpdateFactory.newLatLng(centerLatLong))
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
